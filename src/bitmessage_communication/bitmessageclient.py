ad90fafdb01b3262062f7f4dcb57481e382ebef5
Grzegorz Pstrucha <gpstrucha@gmail.com>
2014-06-01 21:02:14 +0200

    message_id -> ackdata

--- src/bitmessage_communication/bitmessageclient.py
+++ src/bitmessage_communication/bitmessageclient.py
@@ -50,18 +50,18 @@ class BitmessageClient:
     self.get_addresses()
 
   def send_message(self, address, subject, message):
-    message_id = self.api.sendMessage(
-                    address, 
-                    self.default_address.address, 
-                    base64.encodestring(subject),
-                    base64.encodestring(message))
+    ack_data = self.api.sendMessage(
+                  address, 
+                  self.default_address.address, 
+                  base64.encodestring(subject),
+                  base64.encodestring(message))
     return message_id
 
   def send_broadcast(self, subject, message):
-    message_id = self.api.sendBroadcast(
-                    self.default_address.address,
-                    base64.encodestring(subject),
-                    base64.encodestring(message))
+    ack_data = self.api.sendBroadcast(
+                  self.default_address.address,
+                  base64.encodestring(subject),
+                  base64.encodestring(message))
     return message_id
 
   def delete_address(self, address):

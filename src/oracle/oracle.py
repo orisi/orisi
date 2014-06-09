6d32f00c28befec0f83da63d2aa3c5900bc3150c
Grzegorz Pstrucha <gpstrucha@gmail.com>
2014-06-09 16:46:37 +0200

    don't revert my changes

--- src/oracle/oracle.py
+++ src/oracle/oracle.py
@@ -38,37 +38,43 @@ class Oracle:
     condition = body['condition']
     # Future reference - add parsing condition. Now assumed true
     if not self.condition_valid(condition):
+      logging.debug("condition invalid")
       return
 
     transaction = body['raw_transaction']
     prevtx = body['prevtx']
     if not self.transaction_valid(transaction):
+      logging.debug("transaction invalid")
       return
 
     if not self.btc.transaction_need_signature(transaction):
+      logging.debug("transaction does not need a signature")
       return
 
     if not self.btc.transaction_contains_oracle_fee(transaction):
+      logging.debug("oracle fee not found")
       self.communication.broadcast(SUBJECT.NO_FEE, RESPONSE.NO_FEE)
       return
 
     if self.btc.transaction_already_signed(transaction, prevtx):
+      logging.debug("transaction already signed")
       return
 
-    inputs_outputs = self.btc.get_inputs_outputs(transaction)
-    multisig_address = self.btc.get_multisig_sender_address(transaction)
+    inputs, output = self.btc.get_inputs_outputs(transaction)
 
-    used_address_db = UsedAddress(self.db)
-    used_address = used_address_db.get_address(multisig_address)
-    if used_address:
-      #DANGER! SHOULD BE TESTED AND PREPARED OMG!
-      #checking equality should be done key by key, json object does not preserve order, json list does
-      if used_address["json_in_out"] != inputs_outputs:
-        return
-    else:
-      used_address_db.save({
-          "multisig_address": multisig_address,
-          "json_in_out": inputs_outputs,
+    used_input_db = UsedInput(self.db)
+    for i in inputs:
+      used_input = used_input_db.get_input(i)
+      if used_input:
+        if used_addres["json_out"] != output:
+          self.broadcast(
+              SUBJECT.ADDRESS_DUPLICATE,
+              RESPONSE.ADDRESS_DUPLICATE)
+          return
+    for i in inputs:
+      used_input_db.save({
+          'input_hash': i,
+          'json_out': output
       })
 
     locktime = int(body['locktime'])

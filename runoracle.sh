52d7cf5202d70275831a71aedefdfd5700bd2686
Tomasz Kolinko <kolinko@gmail.com>
2014-06-23 12:48:48 +0200

    python2 -> python27

--- runoracle.sh
+++ runoracle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python2 PyBitmessage/src/bitmessagemain.py > /dev/null &
+python27 PyBitmessage/src/bitmessagemain.py > /dev/null &
 bitcoin/bin/32/bitcoind -connect=127.0.0.1 &
 sleep 5
-python2 src/run_oracle.py
+python27 src/run_oracle.py

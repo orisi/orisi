bf7b420bea9b158a1c3b3f104d810e77073963f7
Grzegorz Pstrucha <gpstrucha@gmail.com>
2014-06-24 23:50:24 +0200

    Change styles in BaseHandler file

--- src/oracle/handlers/basehandler.py
+++ src/oracle/handlers/basehandler.py
@@ -1,11 +1,8 @@
-
-
 class BaseHandler:
-
-  def handle_task(self, task):
+  def handle_request(self, task):
     raise NotImplementedError()
 
-  def handle_request(self, task):
+  def handle_task(self, task):
     raise NotImplementedError()
 
   def filter_tasks(self, task):

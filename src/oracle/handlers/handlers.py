1b2b7289d18fd050d70f10c2af5da896859a820e
Grzegorz Pstrucha <gpstrucha@gmail.com>
2014-06-26 00:25:13 +0200

    Init guess password

--- src/oracle/handlers/handlers.py
+++ src/oracle/handlers/handlers.py
@@ -1,7 +1,9 @@
 from conditionedtransactionhandler import ConditionedTransactionHandler
 from password_transaction.passwordtransactionrequesthandler import PasswordTransactionRequestHandler
+from password_transaction.guesspasswordhandler import GuessPasswordHandler
 
 handlers = {
     'conditioned_transaction': ConditionedTransactionHandler,
-    'password_transaction': PasswordTransactionRequestHandler
+    'password_transaction': PasswordTransactionRequestHandler,
+    'guess_password': GuessPasswordHandler,
 }
---
+++ src/oracle/handlers/password_transaction/guesspasswordhandler.py
@@ -0,0 +1,11 @@
+from basehandler import BaseHandler
+
+class GuessPasswordHandler(BaseHandler):
+  def handle_request(self, request):
+    pass
+
+  def handle_task(self, task):
+    pass
+
+  def filter_tasks(self, task):
+    return [task]

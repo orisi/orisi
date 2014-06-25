1a1e5a7fe8731991d381cf67a81d5c7e8dc977d6
Alex Cabrera <alex.cabrera@gmail.com>
2014-06-25 16:16:56 -0400

    use different ports, data in the install dir

--- installoracle.sh
+++ installoracle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PYTHON_EXEC=python27
+PYTHON_EXEC=python2
 
 wget https://bitcoin.org/bin/0.9.1/bitcoin-0.9.1-linux.tar.gz
 tar -zxvf bitcoin-0.9.1-linux.tar.gz
@@ -11,29 +11,31 @@ git clone git://github.com/Bitmessage/PyBitmessage.git
 
 cp src/settings_local.py.example src/settings_local.py
 
-mkdir ~/.bitcoin/
-touch ~/.bitcoin/bitcoin.conf
+mkdir ./.bitcoin/
+touch ./.bitcoin/bitcoin.conf
 
 BTCRPC=`openssl rand -hex 32`
-echo rpcuser=bitrpc >> ~/.bitcoin/bitcoin.conf
-echo rpcpassword=$BTCRPC >> ~/.bitcoin/bitcoin.conf
+echo rpcuser=bitrpc >> ./.bitcoin/bitcoin.conf
+echo rpcpassword=$BTCRPC >> ./.bitcoin/bitcoin.conf
 echo BITCOIND_RPC_PASSWORD = \"$BTCRPC\" >> src/settings_local.py
 
-$PYTHON_EXEC PyBitmessage/src/bitmessagemain.py > /dev/null &#sleep 5
+
+BITMESSAGE_HOME=$(pwd)/PyBitmessage/
+export BITMESSAGE_HOME
+
+$PYTHON_EXEC PyBitmessage/src/bitmessagemain.py > /dev/null &
 sleep 5
 pkill -x $PYTHON_EXEC
 
-mkdir ~/.config/
-mkdir ~/.config/PyBitmessage/
-
-echo daemon = true >> ~/.config/PyBitmessage/keys.dat
-echo apienabled = true >> ~/.config/PyBitmessage/keys.dat
-echo apiport = 8442 >> ~/.config/PyBitmessage/keys.dat
-echo apiinterface = 127.0.0.1 >> ~/.config/PyBitmessage/keys.dat
-echo apiusername = bitrpc >> ~/.config/PyBitmessage/keys.dat
+echo port = 2522 >> keys.dat
+echo daemon = true >> keys.dat
+echo apienabled = true >> keys.dat
+echo apiport = 2523 >> keys.dat
+echo apiinterface = 127.0.0.1 >> keys.dat
+echo apiusername = bitrpc >> keys.dat
 
 BMPW=`openssl rand -hex 32`
-echo "apipassword = $BMPW" >> ~/.config/PyBitmessage/keys.dat
+echo "apipassword = $BMPW" >> keys.dat
 echo BITMESSAGE_PASSWORD = \"$BMPW\" >> src/settings_local.py
 
 echo "done."
--- runoracle.sh
+++ runoracle.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-PYTHON_EXEC=python27
+PYTHON_EXEC=python2
+
+BITMESSAGE_HOME=$(pwd)/PyBitmessage/
+export BITMESSAGE_HOME
 
 $PYTHON_EXEC PyBitmessage/src/bitmessagemain.py > /dev/null &
-bitcoin/bin/$(getconf LONG_BIT)/bitcoind -connect=127.0.0.1 &
+bitcoin/bin/$(getconf LONG_BIT)/bitcoind -connect=127.0.0.1 -datadir=$(pwd)/.bitcoin/ -port=2520 -rpcport=2521 &
 sleep 5
 $PYTHON_EXEC src/run_oracle.py

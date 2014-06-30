20aded83606e04682a6a4d3ec1d84e8251737517
Tomasz Kolinko <kolinko@gmail.com>
2014-06-30 17:12:54 +0200

    added full path of settings_local to a diagnostic message

--- src/oracle/oracle.py
+++ src/oracle/oracle.py
@@ -89,7 +89,7 @@ class Oracle:
 
     if not ORACLE_ADDRESS:
       new_addr = self.btc.server.getnewaddress()
-      logging.error("first run? add '%s' to ORACLE_ADDRESS in settings_local.py" % new_addr)
+      logging.error("first run? add '%s' to ORACLE_ADDRESS in src/settings_local.py" % new_addr)
       exit()
 
     logging.info("my multisig address is %s" % ORACLE_ADDRESS)

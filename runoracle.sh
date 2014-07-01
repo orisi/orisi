f70f8e2a7089704bd68821f69ed3c0b0bb1f13e7
Tomasz Kolinko <kolinko@gmail.com>
2014-07-01 12:08:07 +0200

    path fixes in runoracle

--- runoracle.sh
+++ runoracle.sh
@@ -2,18 +2,19 @@
 
 PYTHON_EXEC=python2.7
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+HOME="$DIR/.."
 
 if [ -z $(pgrep $PYTHON_EXEC) ]
 then
-    BITMESSAGE_HOME=$DIR/PyBitmessage/
+    BITMESSAGE_HOME=$HOME/PyBitmessage/
     export BITMESSAGE_HOME
-    $PYTHON_EXEC $DIR/PyBitmessage/src/bitmessagemain.py > bm_error.log &
+    $PYTHON_EXEC $HOME/PyBitmessage/src/bitmessagemain.py > bm_error.log &
     sleep 2
 fi
 
 if [ -z $(pgrep bitcoind) ]
 then
-    $DIR/bitcoin/bin/$(getconf LONG_BIT)/bitcoind -connect=127.0.0.1 -datadir=$DIR/.bitcoin/ -rpcport=2521 &
+    $HOME/bitcoin/bin/$(getconf LONG_BIT)/bitcoind -connect=127.0.0.1 -datadir=$HOME/.bitcoin/ -rpcport=2521 &
     sleep 2
 fi
 

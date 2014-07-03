9841768ccdfa5f0028d9cbdb5e9f0735e573bdeb
Tomasz Kolinko <kolinko@gmail.com>
2014-07-03 19:33:04 +0200

    guesspasswordhandler -> bounty_redeem_handler

--- src/oracle/handlers/handlers.py
+++ src/oracle/handlers/handlers.py
@@ -1,6 +1,6 @@
 from conditionedtransactionhandler import ConditionedTransactionHandler
 from bounty_contract.bounty_create_handler import PasswordTransactionRequestHandler
-from bounty_contract.guesspasswordhandler import GuessPasswordHandler
+from bounty_contract.bounty_redeem_handler import GuessPasswordHandler
 
 handlers = {
     'conditioned_transaction': ConditionedTransactionHandler,

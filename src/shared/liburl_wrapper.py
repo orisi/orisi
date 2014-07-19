379e7b8e55a27d40fd55f22c90cd8070450aff2a
Grzegorz Pstrucha <gpstrucha@gmail.com>
2014-07-19 03:05:58 +0200

    Styling fix: @kolinko, please be consistent, use spaces for indents

--- src/shared/liburl_wrapper.py
+++ src/shared/liburl_wrapper.py
@@ -25,13 +25,13 @@ def safe_read(url, timeout_time):
 def safe_pushtx(tx, timeout_time = 10):
   signal.setitimer(signal.ITIMER_REAL, timeout_time)
   try:
-	#thanks http://www.pythonforbeginners.com/python-on-the-web/how-to-use-urllib2-in-python/
-  	query_args = {'send': 'Push', 'transaction': tx}
-	data = urllib.urlencode(query_args)
-	url = 'http://eligius.st/~wizkid057/newstats/pushtxn.php'
-	content = urllib2.urlopen(url, data, timeout=timeout_time).read()
-	signal.setitimer(signal.ITIMER_REAL, 0)
-	return content
+    #thanks http://www.pythonforbeginners.com/python-on-the-web/how-to-use-urllib2-in-python/
+    query_args = {'send': 'Push', 'transaction': tx}
+    data = urllib.urlencode(query_args)
+    url = 'http://eligius.st/~wizkid057/newstats/pushtxn.php'
+    content = urllib2.urlopen(url, data, timeout=timeout_time).read()
+    signal.setitimer(signal.ITIMER_REAL, 0)
+    return content
   except:
     signal.setitimer(signal.ITIMER_REAL, 0)
     return None

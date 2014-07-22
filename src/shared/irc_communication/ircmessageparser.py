a0a8151e4e03f13613cd6693f7d453caa27cf388
Grzegorz Pstrucha <gpstrucha@gmail.com>
2014-07-22 22:20:58 +0200

    IrcMessageParser -> extract_message

--- src/shared/irc_communication/ircmessageparser.py
+++ src/shared/irc_communication/ircmessageparser.py
@@ -11,7 +11,7 @@ class IrcMessageParser:
     Takes message and prepares it to send via IRC.
     Arguments: string with message
     Return value: List of strings with given format:
-    <msg_id>,<part_number>,<base64_encoded_message_part>
+    <msg_id>#<part_number>#<number_of_last_part>#<base64_encoded_message_part>
     """
 
     message_id = str(uuid.uuid4())
@@ -22,7 +22,7 @@ class IrcMessageParser:
     factor = 1
     # Loop is responsible for determining how many digits we need for 'part_number' field
     while 1:
-      available_length_per_message -= 1
+      available_length_per_message -= 2
       factor *= 10
 
       if msg_length / available_length_per_message <= factor:
@@ -33,6 +33,50 @@ class IrcMessageParser:
 
     result = []
     for idx, chunk in enumerate(message_chunks):
-      result.append("{}#{}#{}".format(message_id, idx, chunk))
+      result.append("{}#{}#{}#{}".format(message_id, idx, len(message_chunks) - 1, chunk))
 
     return result
+
+  @staticmethod
+  def extract_message(message_chunks):
+    """
+    Takes list of messages chunks corresponding to protocol from above function:
+    <msg_id>#<part_number>#<number_of_last_part>#<base64_encoded_message_part>
+    Returns string with message hidden in that list
+
+    If list of messages has some errors (invalid base64 message, not all the same msg_id, or other error)
+    function will return None. The order of parts doesn't matter
+    """
+
+    chunks = [{"id":ch[0], "part":int(ch[1]), "last_part":int(ch[2]), "msg":ch[3]} for ch in [ch.split('#') for ch in message_chunks]]
+
+    if len(chunks) == 0:
+      return None
+
+    ids = set()
+    for c in chunks:
+      ids.add(c['id'])
+
+    if len(list(ids)) != 1:
+      return None
+
+    if len(chunks) - 1 != chunks[0]['last_part']:
+      print "guwno 2"
+      print chunks
+      return None
+
+    message_parts = sorted([(ch['part'], ch['msg']) for ch in chunks])
+    message_parts = [m[1] for m in message_parts]
+
+    full_string = ''.join(message_parts)
+    try:
+      message = base64.decodestring(full_string)
+    except:
+      # Probably corrupted string
+      print "guwno 3"
+      print full_string
+      print message_parts
+      return None
+
+    return message
+
